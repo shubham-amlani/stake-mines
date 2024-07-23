8ae3a610c6d63d01725d4c370f241f0f527795d1
Shubham Amlani <shubhamamlani104@gmail.com>
2024-07-23 19:25:10 +0530

    Made some changes in the Disclamer component

--- src/components/Disclamer.jsx
+++ src/components/Disclamer.jsx
@@ -5,7 +5,10 @@ const Disclamer = () => {
     <>
     <h3 className='text-red-500 text-lg font-medium text-center'>Disclamer</h3>
     <div className='text-sm text-white font-medium w-[85%] mx-auto mb-5 mt-0'>
-      This project is a non-commercial clone of the Stake Mines game, created purely for educational and hobby purposes. It utilizes assets such as sounds, images, and design elements from the original Stake Mines game. The functionality mirrors that of the Stake Mines game; however, no real money is involved. This clone is not intended for commercial use in any form.
+      This project is a non-commercial clone of the Stake Mines game, created purely for educational and hobby purposes. It utilizes assets such as sounds, images, and design elements from the original Stake Mines game. The functionality mirrors that of the Stake Mines game; however, no real money is involved. This clone is not intended for commercial use in any form.<br/><br />
+      For any queries please contact: shubhamamlani104@gmail.com <br/>
+      Developed by: <b className='text-green-500'>Shubham Amlani</b> <br/>
+      <b className="text-red-500">Note:- Each code belongs to the developer and should not be used/claimed without permission.</b>
     </div>
     </>
   )
